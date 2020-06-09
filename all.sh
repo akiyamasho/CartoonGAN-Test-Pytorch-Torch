b71c25090f9f19f1c9f1f9d6eb83c2416465cff5
秋山翔 <35907066+akiyamasho@users.noreply.github.com>
2020-06-10 00:55:17 +0900

    chore: minor scripts and README

---
+++ all.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+python test.py --input_dir test_img --style Paprika --gpu -1
+python test.py --input_dir test_img --style Hosoda --gpu -1
+python test.py --input_dir test_img --style Hayao --gpu -1
+python test.py --input_dir test_img --style Shinkai --gpu -1
--- shinkai.sh
+++ shinkai.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python test.py --input_dir test_img --style Shinkai --gpu -1
+python test.py --input_dir test_img --style Hosoda --gpu -1
